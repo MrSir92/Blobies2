39866f6159950eb1ba693315cdeaa5e3a935965b
MrSir92 <krisoffer.sor@yhumea.net>
2015-10-07 11:07:00 +0200

    second
    ayylmao

--- Blobies2/Blobies2/GameScene.swift
+++ Blobies2/Blobies2/GameScene.swift
@@ -33,15 +33,13 @@ class GameScene: SKScene {
         self.TheLevel.spawnBlobNode(CGPoint(x: 50, y: 150))
         
     }
-    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
         for touch in touches {
             let location = touch.locationInNode(self)
             
-            //CGPathMoveToPoint(ref, nil, location.x, location.y)
-            CGPathMoveToPoint(, <#T##m: UnsafePointer<CGAffineTransform>##UnsafePointer<CGAffineTransform>#>, <#T##x: CGFloat##CGFloat#>, <#T##y: CGFloat##CGFloat#>)
+            CGPathMoveToPoint(ref , nil, location.x, location.y)
             // Select the sprite where the touch occurred.
             var isSprite = checkIfNodeIsSprite(location)
             
@@ -54,6 +52,48 @@ class GameScene: SKScene {
             
             
         }
+    }
+    var ref = CGPathCreateMutable()
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        var touch =  touches.first
+        
+        //var positionInScene = touch.locationInNode(self)
+        var positionInScene = touch!.locationInNode(self)
+        
+        if (self.globalBool) {
+            //TODO: Set the lengthlimit.
+            //TODO: Check if the lengthlimit has been reached:
+            //TODO: After each for-loop, check how long the subpath is.
+            //TODO: If the total length of the subpath is reached, don't add the
+            //latest subpath and end the for-loop.
+            
+            for touch: AnyObject in touches {
+                let locationInScene = touch.locationInNode(self)
+                CGPathAddLineToPoint(ref, nil, positionInScene.x, positionInScene.y)
+                
+            }
+            
+        }
+    }
+    
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        var lineNode = SKShapeNode();
+        lineNode.path = ref
+        lineNode.lineWidth = 8
+        lineNode.strokeColor = UIColor.redColor()
+        lineNode.physicsBody = SKPhysicsBody(polygonFromPath: ref)
+        self.addChild(lineNode)
+        ref = CGPathCreateMutable()
+    }
+    
+    
+    func checkIfNodeIsSprite(location: CGPoint) ->Bool {
+        if let touchedNode = self.nodeAtPoint(location) as? SKSpriteNode {
+            print("Selected sprite with name: " + touchedNode.name!)
+            return true
+        }
+        return false
     }
    
     override func update(currentTime: CFTimeInterval) {
