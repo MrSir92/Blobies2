f219b0a41d6fe1a202ca9c3fe922e7e89835efb7
MrSir92 <krisoffer.sor@yhumea.net>
2015-10-09 15:31:05 +0200

    Checkwallcollision

--- Blobies2/Blobies2/Blob.swift
+++ Blobies2/Blobies2/Blob.swift
@@ -10,6 +10,9 @@ import UIKit
 import SpriteKit
 
 class BlobNode: SKSpriteNode {
+    
+    internal var positions = [CGFloat]()
+    
     class func blob(location: CGPoint) -> BlobNode {
         let sprite = BlobNode(imageNamed:"blob.png")
         
@@ -21,4 +24,35 @@ class BlobNode: SKSpriteNode {
         return sprite
     }
     
+    func Update(){
+        
+    }
+    
+    func checkWallCollide(position: CGFloat) -> Bool {
+        
+        var sum = CGFloat(0)
+    
+        for i in(9...0){
+            positions[i+1] = positions[i]
+        }
+        positions.removeAtIndex(10)
+        
+        positions[0] = position
+        
+        for i in(0...9){
+            sum = sum + positions[i]
+        }
+        
+        
+        if sum/CGFloat(positions.count) < positions[0] + CGFloat(5)
+            || sum/CGFloat(positions.count) > positions[0] - CGFloat(5) {
+                
+            return true;
+                
+        }
+        else{
+            return false;
+        }
+
+    }
 }
