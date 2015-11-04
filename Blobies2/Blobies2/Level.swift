6b7f97ee53d9cb4b358d857495af0b1a709adcd9
MrSir92 <krisoffer.sor@yhumea.net>
2015-11-04 16:26:43 +0100

    platform thingie

--- Blobies2/Blobies2/Level.swift
+++ Blobies2/Blobies2/Level.swift
@@ -35,102 +35,69 @@ class Level: SKNode {
         switch(progress){
         case 1:
             
-            //let darkBrownColor = UIColor(red:0.4, green:0.3, blue:0.2, alpha:1);
             let lightBrownColor = UIColor(red:0.6, green:0.5, blue:0.4, alpha:1);
-            //self.backgroundColor = darkBrownColor;
+            
             let spawnPoint = SKShapeNode(rectOfSize: CGSize(width: 30, height: 200))
             spawnPoint.name = "spawnPoint"
             spawnPoint.fillColor = SKColor(red: 0, green: 0.7, blue: 0, alpha: 1)
             spawnPoint.position = CGPoint(x: 50, y: 310)
             World.addChild(spawnPoint)
-            let roof1 = SKShapeNode(rectOfSize: CGSize(width: 150, height: 300));
-            roof1.name = "roof1";
+            
+            let roof1 = platform("roof1", pos: CGPoint(x:74, y:400), rectSize: CGSize(width: 150, height: 300))
             roof1.fillColor = lightBrownColor;
-            roof1.position = CGPoint(x: 74, y: 400);
-            roof1.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 150, height: 300))
-            roof1.physicsBody?.dynamic = false;
             roof1.physicsBody?.categoryBitMask = CollisionTypes.Wall.rawValue
             World.addChild(roof1);
-            let wall1 = SKShapeNode(rectOfSize: CGSize(width: 20, height: 140));
-            wall1.name = "wall1";
+            
+            let wall1 = platform("wall1", pos: CGPoint(x: 9, y: 180), rectSize: CGSize(width: 20, height: 140))
             wall1.fillColor = lightBrownColor;
-            wall1.position = CGPoint(x: 9, y: 180);
-            wall1.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 20, height: 140))
-            wall1.physicsBody?.dynamic = false;
             wall1.physicsBody?.categoryBitMask = CollisionTypes.Wall.rawValue
             World.addChild(wall1);
-            let ground1 = SKShapeNode(rectOfSize: CGSize(width: 200, height: 30));
-            ground1.name = "ground1";
+            
+            let ground1 = platform("ground1", pos: CGPoint(x: 99,y: 110), rectSize: CGSize(width: 200, height: 30))
             ground1.fillColor = lightBrownColor;
-            ground1.position = CGPoint(x: 99, y: 110);
-            ground1.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 200, height: 30))
-            ground1.physicsBody?.dynamic = false;
             ground1.physicsBody?.categoryBitMask = CollisionTypes.Wall.rawValue
             World.addChild(ground1);
-            let ground2 = SKShapeNode(rectOfSize: CGSize(width: 30, height: 30));
-            ground2.name = "ground1";
+            
+            let ground2 = platform("ground2", pos: CGPoint(x: 245, y: 110), rectSize: CGSize(width: 30, height: 30))
             ground2.fillColor = lightBrownColor;
-            ground2.position = CGPoint(x: 245, y: 110);
-            ground2.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 30, height: 30))
-            ground2.physicsBody?.dynamic = false;
             ground2.physicsBody?.categoryBitMask = CollisionTypes.Wall.rawValue
             World.addChild(ground2);
-            let ground3 = SKShapeNode(rectOfSize: CGSize(width: 100, height: 30))
-            ground3.name = "ground3";
+            
+            let ground3 = platform("ground3", pos: CGPoint(x: 370, y: 110), rectSize: CGSize(width: 100, height: 30))
             ground3.fillColor = lightBrownColor;
-            ground3.position = CGPoint(x: 370, y: 110);
-            ground3.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 100, height: 30))
-            ground3.physicsBody?.dynamic = false;
             ground3.physicsBody?.categoryBitMask = CollisionTypes.Wall.rawValue
             World.addChild(ground3)
-            let ground4 = SKShapeNode(rectOfSize: CGSize(width: 50, height: 30))
-            ground4.name = "ground4";
+            
+            let ground4 = platform("ground4", pos: CGPoint(x: 475, y: 110), rectSize: CGSize(width: 50, height: 30))
             ground4.fillColor = lightBrownColor;
-            ground4.position = CGPoint(x: 475, y: 110);
-            ground4.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 50, height: 30))
-            ground4.physicsBody?.dynamic = false;
             ground4.physicsBody?.categoryBitMask = CollisionTypes.Wall.rawValue
             World.addChild(ground4)
-            let ground5 = SKShapeNode(rectOfSize: CGSize(width: 150, height: 60))
-            ground5.name = "ground5";
+
+            let ground5 = platform("ground5", pos: CGPoint(x: 575, y: 125), rectSize: CGSize(width: 150, height: 60))
             ground5.fillColor = lightBrownColor;
-            ground5.position = CGPoint(x: 575, y: 125);
-            ground5.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 150, height: 60))
-            ground5.physicsBody?.dynamic = false;
             ground5.physicsBody?.categoryBitMask = CollisionTypes.Wall.rawValue
             World.addChild(ground5)
-            let ground6 = SKShapeNode(rectOfSize: CGSize(width: 150, height: 30))
-            ground6.name = "ground6";
+            
+            let ground6 = platform("ground6", pos: CGPoint(x: 825, y: 110), rectSize: CGSize(width: 150, height: 30))
             ground6.fillColor = lightBrownColor;
-            ground6.position = CGPoint(x: 825, y: 110);
-            ground6.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 150, height: 30))
-            ground6.physicsBody?.dynamic = false;
             ground6.physicsBody?.categoryBitMask = CollisionTypes.Wall.rawValue
             World.addChild(ground6)
-            let wall2 = SKShapeNode(rectOfSize: CGSize(width: 20, height: 230))
-            wall2.name = "wall2";
+            
+            let wall2 = platform("wall2", pos: CGPoint(x: 910, y: 210), rectSize: CGSize(width: 20, height: 230))
             wall2.fillColor = lightBrownColor;
-            wall2.position = CGPoint(x: 910, y: 210);
-            wall2.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 20, height: 230))
-            wall2.physicsBody?.dynamic = false;
             wall2.physicsBody?.categoryBitMask = CollisionTypes.Wall.rawValue
             World.addChild(wall2)
+            
             print("levelstarted")
-            let deathGround = SKShapeNode(rectOfSize: CGSize(width: 1000, height: 30))
-            deathGround.name = "deathGround"
-            deathGround.position = CGPoint(x: 500, y: 10)
-            deathGround.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 1000, height: 30))
-            deathGround.physicsBody?.dynamic = false
+            
+            let deathGround = platform("deathGround", pos: CGPoint(x: 500, y: 10), rectSize: CGSize(width: 1000, height: 30))
             deathGround.alpha = 0
             deathGround.physicsBody?.categoryBitMask = CollisionTypes.Death.rawValue
             deathGround.physicsBody?.contactTestBitMask = CollisionTypes.Blob.rawValue
             World.addChild(deathGround)
-            let finishLine = SKShapeNode(rectOfSize: CGSize(width: 30, height: 30))
-            finishLine.name = "finishLine"
-            finishLine.position = CGPoint(x: 870, y: 140)
+            
+            let finishLine = platform("finishLine", pos: CGPoint(x: 870, y: 140), rectSize: CGSize(width: 30, height: 30))
             finishLine.fillColor = SKColor(red: 0.7, green: 0.7, blue: 0, alpha: 1)
-            finishLine.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 30, height: 30))
-            finishLine.physicsBody?.dynamic = false
             finishLine.physicsBody?.categoryBitMask = CollisionTypes.Finish.rawValue
             finishLine.physicsBody?.contactTestBitMask = CollisionTypes.Blob.rawValue
             World.addChild(finishLine)
@@ -138,6 +105,17 @@ class Level: SKNode {
             print("wrong level")
         }
         
+    }
+    func platform(name: String, pos: CGPoint,rectSize :CGSize) ->SKShapeNode{
+        let node = SKShapeNode(rectOfSize: rectSize)
+        
+        node.name = name
+        node.position = pos
+        node.physicsBody = SKPhysicsBody(rectangleOfSize: rectSize)
+        node.physicsBody?.dynamic = false
+        
+        
+        return node
     }
     
     
